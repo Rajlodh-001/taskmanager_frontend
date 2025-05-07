8be213cbc49703c67aa5415f202e60feac537359
Rajlodh-001 <Rajlodh911@gmail.com>
2025-05-07 19:33:02 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="bg-red-400">App
+    <div className='bg-red-300'>
+      fd
+    </div>
+    <div>
+      <h1 className='text-'>Hello</h1>
+    </div>
+    </div>
+  )
+}
+
+export default App
